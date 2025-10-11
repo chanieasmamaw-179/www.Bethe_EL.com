41a8c4138f8ce682d121c4b8fdd0b5c47bbeb4d0
chanieasmamaw-179 <chanieasmamaw@yahoo.com>
2025-10-11 17:36:10 +0200

    updates
    # Conflicts:
    #	.env
    #	app.py

---
+++ gunicorn_config.py
@@ -0,0 +1,34 @@
+# Gunicorn configuration file
+import multiprocessing
+
+# Server socket
+bind = "0.0.0.0:10000"
+backlog = 2048
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'sync'
+worker_connections = 1000
+timeout = 60  # Increased from default 30 seconds
+keepalive = 5
+graceful_timeout = 30
+
+# Logging
+accesslog = '-'
+errorlog = '-'
+loglevel = 'info'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+
+# Process naming
+proc_name = 'bethe_el_app'
+
+# Server mechanics
+daemon = False
+pidfile = None
+umask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+# Preload app for better performance
+preload_app = True
